b1d42fc751031e61f5ad79124f4c44b7968269a9
Duy-Kun <sherlockduy2001@gmail.com>
2022-04-04 15:33:41 +0700

    Encoding password

--- DAL/AccountDAL.cs
+++ DAL/AccountDAL.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace QuanlyCanHoGiangTran.DAL
 {
@@ -18,8 +19,17 @@ namespace QuanlyCanHoGiangTran.DAL
         private AccountDAL() { }
         public bool Login(string userName, string passWord)
         {
+            byte[] temp = ASCIIEncoding.ASCII.GetBytes(passWord);
+            byte[] hasData = new MD5CryptoServiceProvider().ComputeHash(temp);
+
+            string hasPass = "";
+            foreach (byte item in hasData)
+            {
+                hasPass += item;
+            }
+
             string query = "EXEC dbo.Login @userName , @passWord";
-            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] { userName, passWord });
+            DataTable result = DataProvider.Instance.ExecuteQuery(query, new object[] { userName, hasPass });
             return result.Rows.Count > 0;
         }
     }
